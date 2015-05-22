a72a457e126be579acb1b041c06175c281f3fc72
robertburrowes <r.burrowes@auckland.ac.nz>
2015-05-22 14:55:39 +1200

    Changed to test for Siberia in May

--- test/manualtest.rb
+++ test/manualtest.rb
@@ -1,7 +1,10 @@
 #!/usr/local/bin/ruby
 
 require '../lib/sunriseset.rb'
+require 'date'
 #require 'sunriseset'
 
-puts SunRiseSet.now
+puts SunRiseSet.new(DateTime.now  , 53.5, 138.9)
+puts '####'
+#puts SunRiseSet.new(DateTime.now  , 53.4564,138.8954)
 
