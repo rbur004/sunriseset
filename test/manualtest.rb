f534bb635a437da3932d30920b9b32353637f8ea
robertburrowes <r.burrowes@auckland.ac.nz>
2016-07-03 19:42:05 +1200

    window dressing

--- test/manualtest.rb
+++ test/manualtest.rb
@@ -7,4 +7,5 @@ require 'date'
 puts SunRiseSet.new(DateTime.now  , 53.5, 138.9)
 puts '####'
 #puts SunRiseSet.new(DateTime.now  , 53.4564,138.8954)
-
+today = SunRiseSet.new(DateTime.now,-36.991,174.487)
+puts today
